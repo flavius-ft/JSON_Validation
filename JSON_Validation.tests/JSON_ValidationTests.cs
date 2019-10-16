e825d58a5c9ddddbdfc3db129a29b7fd725998d3
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2019-10-16 17:20:28 +0300

    test 2

--- JSON_Validation.tests/JSON_ValidationTests.cs
+++ JSON_Validation.tests/JSON_ValidationTests.cs
@@ -12,6 +12,7 @@ namespace JSON_Validation.tests
             string jsonString = string.Empty;
             Assert.True(Program.ValidateJsonString(jsonString));
             bun SA CRA CE MAI FACI
+                Hi acasa
         }
     }
 }
